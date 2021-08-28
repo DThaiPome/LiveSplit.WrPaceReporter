aa0a9dfbc1614d3fb56e8acb97351e44eb70cc5d
DThaiPome <r.gehrlein154@gmail.com>
2021-08-28 16:02:26 -0400

    Initial setup

---
+++ LiveSplit.WrPaceReporter/WrPaceReporter.cs
@@ -0,0 +1,76 @@
+﻿using LiveSplit.Model;
+using LiveSplit.UI;
+using LiveSplit.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+using System.Xml;
+using System.Windows.Forms;
+
+namespace LiveSplit
+{
+    public class WrPaceReporter : IComponent
+    {
+        public string ComponentName => "WrPace Reporter";
+
+        public float HorizontalWidth => 0f;
+
+        public float MinimumHeight => 0f;
+
+        public float VerticalHeight => 0f;
+
+        public float MinimumWidth => 0f;
+
+        public float PaddingTop => 0f;
+
+        public float PaddingBottom => 0f;
+
+        public float PaddingLeft => 0f;
+
+        public float PaddingRight => 0f;
+
+        public IDictionary<string, Action> ContextMenuControls => null;
+
+        public void Dispose()
+        {
+            return;
+        }
+
+        public void DrawHorizontal(System.Drawing.Graphics g, LiveSplitState state, float height, System.Drawing.Region clipRegion)
+        {
+            return;
+        }
+
+        public void DrawVertical(System.Drawing.Graphics g, LiveSplitState state, float width, System.Drawing.Region clipRegion)
+        {
+            return;
+        }
+
+        public XmlNode GetSettings(XmlDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Control GetSettingsControl(LayoutMode mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSettings(XmlNode settings)
+        {
+            return;
+        }
+
+        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+            return;
+        }
+    }
+}
---
+++ LiveSplit.WrPaceReporter/WrPaceReporterFactory.cs
@@ -0,0 +1,32 @@
+﻿using LiveSplit.Model;
+using LiveSplit.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveSplit
+{
+    public class WrPaceReporterFactory : IComponentFactory
+    {
+        public string ComponentName => "WrPace Reporter";
+
+        public string Description => "Notify members of the speedrunning community when you are on pace for a world record in your category!";
+
+        public ComponentCategory Category => ComponentCategory.Other;
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "";
+
+        public string UpdateURL => "";
+
+        public Version Version => new Version("0.0.1");
+
+        public IComponent Create(LiveSplitState state)
+        {
+            return new WrPaceReporter();
+        }
+    }
+}
